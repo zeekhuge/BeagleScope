d57475e286c93e8d65150c625d166f4643aa7e65
ZeekHuge <me.zubeen3@gmail.com>
2016-06-21 16:03:47 +0000

    Add example PRU_inline_asm_blinky The example is to a very basic demonstration of how we can develope time critical part of code in pru assembly, and can integrate it with C code. This way, we can take advantage of both worlds

---
+++ examples/firmware_exmples/PRU_inline_asm_blinky/pru1-asm-blinky.asm
@@ -0,0 +1,32 @@
+;*
+;* Copyright (C) 2016 Zubeen Tolani <ZeekHuge - zeekhuge@gmail.com>
+;*
+;* This file is as an example to show how to develope
+;* and compile inline assembly code for PRUs
+;*
+;* This program is free software; you can redistribute it and/or modify
+;* it under the terms of the GNU General Public License version 2 as
+;* published by the Free Software Foundation.
+
+
+	.cdecls "main.c"
+
+DELAY	.macro time
+	LDI32	R0, time
+	QBEQ	$E?, R0, 0
+$M?:	SUB	R0, R0, 1
+	QBNE	$M?, R0, 0
+$E?:	
+	.endm
+	
+
+	.clink
+	.global start
+start:
+	LDI32 	R30, 0xFFFFFFFF
+	DELAY 	100000000
+	LDI32 	R30, 0x00000000
+	DELAY 	100000000
+	JMP	start	
+
+	HALT
