7af3b72640379ca322ed5767a8671c7385ff58dc
ZeekHuge <me.zubeen3@gmail.com>
2016-06-25 17:06:03 +0000

    Cleanup Cleaned up things like generated files, swap files and anything that was occupying unnecessary space

--- examples/firmware_exmples/PRU_inline_asm_blinky/gen/main.pp
+++
@@ -1,7 +0,0 @@
-gen/main.object: main.c
-gen/main.object: /usr/share/ti/cgt-pru/include/stdint.h
-gen/main.object: ../../include/am335x/pru_cfg.h
-gen/main.object: resource_table_empty.h
-gen/main.object: /usr/share/ti/cgt-pru/include/stddef.h
-gen/main.object: ../../include/rsc_types.h
-gen/main.object: ../../include/pru_types.h
--- examples/firmware_exmples/pru_blinky/PRU_gpioToggle/gen/PRU_gpioToggle.pp
+++
@@ -1,7 +0,0 @@
-gen/PRU_gpioToggle.object: PRU_gpioToggle.c
-gen/PRU_gpioToggle.object: /usr/share/ti/cgt-pru/include/stdint.h
-gen/PRU_gpioToggle.object: ../../include/am335x/pru_cfg.h
-gen/PRU_gpioToggle.object: resource_table_empty.h
-gen/PRU_gpioToggle.object: /usr/share/ti/cgt-pru/include/stddef.h
-gen/PRU_gpioToggle.object: ../../include/rsc_types.h
-gen/PRU_gpioToggle.object: ../../include/pru_types.h
