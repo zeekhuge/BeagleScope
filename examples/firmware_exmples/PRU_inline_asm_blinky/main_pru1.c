24250f8dfcfd68b2164a9c8a6294170e9163da89
ZeekHuge <me.zubeen3@gmail.com>
2016-06-25 19:02:06 +0000

    Rename Renamed a few files and made associated changes main.c -> main_pru1.c resource_table_empty.h -> resource_table_pru1.h

--- examples/firmware_exmples/PRU_inline_asm_blinky/main_pru1.c
+++ examples/firmware_exmples/PRU_inline_asm_blinky/main_pru1.c
@@ -36,7 +36,7 @@
 				  
 #include <stdint.h>
 #include <pru_cfg.h>
-#include "resource_table_empty.h"
+#include "resource_table_pru1.h"
 
 // The function is defined in pru1_asm_blinky.asm in same dir
 // We just need to add a declaration here, the defination can be
