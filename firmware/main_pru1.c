63198e16bc0b505896e20a38d3d2579703989c58
ZeekHuge <me.zubeen3@gmail.com>
2016-06-28 16:11:21 +0000

    Add files for pru-core1 The main_pru1.c file is used to declare the main function as external while main_pru1_def.asm provides with the defination of this main function

---
+++ firmware/main_pru1.c
@@ -0,0 +1,59 @@
+/*
+ * Source Modified by Zubeen Tolani < ZeekHuge - zeekhuge@gmail.com >
+ * Based on the examples distributed by TI
+ *
+ * Copyright (C) 2015 Texas Instruments Incorporated - http://www.ti.com/
+ *
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *	* Redistributions of source code must retain the above copyright
+ *	  notice, this list of conditions and the following disclaimer.
+ *
+ *	* Redistributions in binary form must reproduce the above copyright
+ *	  notice, this list of conditions and the following disclaimer in the
+ *	  documentation and/or other materials provided with the
+ *	  distribution.
+ *
+ *	* Neither the name of Texas Instruments Incorporated nor the names of
+ *	  its contributors may be used to endorse or promote products derived
+ *	  from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <stdint.h>
+#include "resource_table_pru1.h"
+
+#define CLK_PIN 1<<1
+#define BYTE_1	R1.b0
+#define BYTE_2  R1.b1
+#define BYTE_3  R1.b2
+#define BYTE_4  R1.b3
+#define BYTE_5  R2.b0
+#define BYTE_6  R2.b1
+#define BYTE_7  R2.b2
+#define BYTE_8  R2.b3
+#define BYTE_9  R3.b0
+#define BYTE_10 R3.b1
+#define BYTE_11 R3.b2
+#define BYTE_12 R3.b3
+
+#define FREQ	R31.w0
+#define WIDTH	R3.w1
+register uint32_t __R30;
+volatile register uint32_t __R31;
+
+extern void main(void);
---
+++ firmware/main_pru1_def.asm
@@ -0,0 +1,14 @@
+;*
+;* Copyright (C) 2016 Zubeen Tolani <ZeekHuge - zeekhuge@gmail.com>
+;*
+;* This file is as an example to show how to develope
+;* and compile inline assembly code for PRUs
+;*
+;* This program is free software; you can redistribute it and/or modify
+;* it under the terms of the GNU General Public License version 2 as
+;* published by the Free Software Foundation.
+
+	.global main
+main:
+	
+	HALT
