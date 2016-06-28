3d3732d0195a7229bec3c0a5b561eaafb8d9d022
ZeekHuge <me.zubeen3@gmail.com>
2016-06-28 16:59:17 +0000

    Add basic code Add basic code to the main_pru1_def.asm file. The code is in form of different macros like : DELAY_CYCLES and DELAY_SAMPLE - to cause custom delays INIT -  to initialise things TAKE_SAMPLE_8 - to generate the clock and take sample SAMPLE_CYCLE_8 - one complete cycle to sample 8 bit wide data.

--- firmware/main_pru1_def.asm
+++ firmware/main_pru1_def.asm
@@ -8,6 +8,89 @@
 ;* it under the terms of the GNU General Public License version 2 as
 ;* published by the Free Software Foundation.
 
+	.asg 100000000, CYCLE_BTWN_SAMPLE
+	
+	.cdecls "main_pru1.c"
+
+
+;*******************************************************
+;*******************************************************
+
+DELAY_CYCLES	.macro cycles
+		LDI32	R0, cycles - 1 
+		QBEQ	$E?, R0, 0
+$M?:		SUB	R0, R0, 1
+		QBNE	$M?, R0, 0
+$E?:	
+		.endm
+
+
+
+;*****************************************************
+;*****************************************************
+
+DELAY_SAMPLE	.macro time
+		DELAY_CYCLES time -1 
+		.endm
+;******************************************************
+;******************************************************
+
+INIT	.macro
+	LDI32 R30, 0x00000000
+	.endm
+
+
+;*******************************************************
+;*******************************************************
+
+CLK_TOGGLE	.macro
+		XOR R30, R30, CLK_PIN
+		.endm
+
+
+
+;********************************************************
+;*******************************************************
+
+TAKE_SAMPLE_8	.macro RX
+		CLK_TOGGLE
+		MOV	RX, R31
+		;CLK_TOGGLE
+		.endm
+
+;*********************************************************
+;*********************************************************
+
+SAMPLE_CYCLE_8	.macro bit
+		TAKE_SAMPLE_8	BYTE_1 
+		DELAY_SAMPLE 	CYCLE_BTWN_SAMPLE
+		TAKE_SAMPLE_8	BYTE_2
+		DELAY_SAMPLE	CYCLE_BTWN_SAMPLE
+		TAKE_SAMPLE_8	BYTE_3
+		DELAY_SAMPLE	CYCLE_BTWN_SAMPLE
+		TAKE_SAMPLE_8	BYTE_4
+		DELAY_SAMPLE	CYCLE_BTWN_SAMPLE
+		TAKE_SAMPLE_8   BYTE_5
+                DELAY_SAMPLE    CYCLE_BTWN_SAMPLE
+                TAKE_SAMPLE_8   BYTE_6
+                DELAY_SAMPLE    CYCLE_BTWN_SAMPLE
+                TAKE_SAMPLE_8   BYTE_7
+                DELAY_SAMPLE    CYCLE_BTWN_SAMPLE
+                TAKE_SAMPLE_8   BYTE_8
+                DELAY_SAMPLE    CYCLE_BTWN_SAMPLE
+		TAKE_SAMPLE_8   BYTE_9
+                DELAY_SAMPLE    CYCLE_BTWN_SAMPLE
+                TAKE_SAMPLE_8   BYTE_10
+                DELAY_SAMPLE    CYCLE_BTWN_SAMPLE
+                TAKE_SAMPLE_8   BYTE_11
+                DELAY_SAMPLE    CYCLE_BTWN_SAMPLE
+                TAKE_SAMPLE_8   BYTE_12
+                DELAY_SAMPLE    CYCLE_BTWN_SAMPLE - 1 
+		.endm
+
+;*******************************************************
+;*******************************************************
+
 	.global main
 main:
 	
