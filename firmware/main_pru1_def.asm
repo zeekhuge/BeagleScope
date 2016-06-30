e06bde5e9a1559716159215ad764648ef15ce03d
ZeekHuge <me.zubeen3@gmail.com>
2016-06-30 12:35:19 +0000

    Modify DELAY_SAMPLE and SAMPLE_CYCLE_8
    Add instructions to DELAY_SAMPLE macro. The macro now uses the
    SAMPLING_CONFIG_CYCLE_BTWN_SAMPLE, subtracts 7 and stores the result
    in R0. Further it gets in a loop to cause a delay of R0 (value in R0 )
    cycles.
    
    The subtraction of 7 cycles is because there are separate delays in
    the SAMPLE_CYCLE itself. Thus it turns out that the minimum delay
    between consecutive samples can be 7 and thus least value of
    SAMPLING_CONFIG_CYCLE_BTWN_SAMPLE can be 7. The value in the
    SAMPLING_CONFIG_CYCLE_BTWN_SAMPLE should be exact equal to the delay
    required between 2 consecutive sample, ignoring all delays caused by
    Clock generation or anything. That will be managed in the code itself.
    
    SAMPLE_CYCLE_8 macro was also changed. It is still incomplete and was
    changed to avoid compilation errors due to change in DELAY_SAMPLE
    macro.

--- firmware/main_pru1_def.asm
+++ firmware/main_pru1_def.asm
@@ -10,7 +10,7 @@
 
 	.asg 19, INT_P0_TO_P1
 	.asg 18, INT_P1_TO_P0
-	
+
 	.cdecls "main_pru1.c"
 
 ;*******************************************************
@@ -19,7 +19,7 @@
 ;
 ; The macro essentially consumes one system cycle in
 ; subtracting 0 from the content of R0 register and put
-; the result back in R0 register. This do not changes 
+; the result back in R0 register. This do not changes
 ; anything but just consumes up one single system clock
 ; cycle
 
@@ -39,27 +39,27 @@ NOP	.macro
 ;	macro is 2 cycles
 
 DELAY_IMMEDIATE_2n	.macro cycles_2n
-			LDI32	R0, cycles_2n - 2 
+			LDI32	R0, cycles_2n - 2
 			QBEQ	$E2?, R0, 0
 $M2?:			SUB	R0, R0, 2
 			QBNE	$M2?, R0, 0
-$E2?:	
+$E2?:
 			.endm
 
 
 ;*****************************************************
-; DELAY_IMMEDIATE_3n:	The macro to cause a delay of 
+; DELAY_IMMEDIATE_3n:	The macro to cause a delay of
 ;			'cycles_3n' cycles.
 ;
 ; cycles_3n	: The number of cycles to delay.
 ;		This should be an immediate value and
 ;		should always be a multiple of 3.
-; 
+;
 ; NOTE:	The least delay value that can be given to the
 ; 	macro is 3
 
 DELAY_IMMEDIATE_3n      .macro cycles_3n
-                        LDI32   R0, cycles - 3
+                        LDI32   R0, cycles_3n - 3
                         NOP
                         QBEQ    $E3?, R0, 0
 $M3?:                   SUB     R0, R0, 3
@@ -68,12 +68,35 @@ $M3?:                   SUB     R0, R0, 3
 $E3?:
                         .endm
 
-;*****************************************************
-;*****************************************************
+;********************************************************************
+; DELAY_SAMPLE : To cause delay between samples
+; The macro takes CYCLE_BTWN_SAMPLE value, given by PRU0
+; and uses it to cause a delay between consecutive samples.
+;
+; Instruction: "SUB R0.wo, CYCLE_BTWN_SAMPLE, 3 + 2 + 2"
+; The instrcution takes value from CYCLE_BTWN_SAMPLE value
+; from SAMPLIG.CONFIG data and subtracts 3+2+2 cycles.
+; 3 cycles      = already beign delayed between each sample
+;               in SAMPLE_CYCLE_n macro either by DELAY_CYCLES_3n
+;               macro, or by other macros
+; 2 cycles      = being used by the SUB and QBEQ instructions
+;               in the DELAY_SAMPLE macro
+; 2 cycles      = being used by the 2 CLOCK_TOGGLE instructions
+;               in TAKE_SAMPLE_8 macro
+;
+; NOTE: It turns out that the least value of CYCLE_BTWN_SAMPLE
+;       can be 7. And hence least delay between 2 consecutive
+;       samples will be 7 cycles.
+;
 
-DELAY_SAMPLE	.macro time
- 
-		.endm
+DELAY_SAMPLE    .macro
+                SUB     R0.w0, SAMPLING_CONFIG_CYCLE_BTWN_SAMPLE, 3 + 2 + 2
+                QBEQ    $ES?, R0, 0
+$MS?:           SUB     R0, R0, 2
+                QBNE    $MS?, R0, 0
+$ES?:
+                .endm
+
 ;******************************************************
 ;******************************************************
 
@@ -103,31 +126,53 @@ TAKE_SAMPLE_8	.macro RX
 ;*********************************************************
 ;*********************************************************
 
-SAMPLE_CYCLE_8	.macro bit
-		TAKE_SAMPLE_8	BYTE_1 
-		DELAY_SAMPLE 	CYCLE_BTWN_SAMPLE
-		TAKE_SAMPLE_8	BYTE_2
-		DELAY_SAMPLE	CYCLE_BTWN_SAMPLE
-		TAKE_SAMPLE_8	BYTE_3
-		DELAY_SAMPLE	CYCLE_BTWN_SAMPLE
-		TAKE_SAMPLE_8	BYTE_4
-		DELAY_SAMPLE	CYCLE_BTWN_SAMPLE
-		TAKE_SAMPLE_8   BYTE_5
-                DELAY_SAMPLE    CYCLE_BTWN_SAMPLE
-                TAKE_SAMPLE_8   BYTE_6
-                DELAY_SAMPLE    CYCLE_BTWN_SAMPLE
-                TAKE_SAMPLE_8   BYTE_7
-                DELAY_SAMPLE    CYCLE_BTWN_SAMPLE
-                TAKE_SAMPLE_8   BYTE_8
-                DELAY_SAMPLE    CYCLE_BTWN_SAMPLE
-		TAKE_SAMPLE_8   BYTE_9
-                DELAY_SAMPLE    CYCLE_BTWN_SAMPLE
-                TAKE_SAMPLE_8   BYTE_10
-                DELAY_SAMPLE    CYCLE_BTWN_SAMPLE
-                TAKE_SAMPLE_8   BYTE_11
-                DELAY_SAMPLE    CYCLE_BTWN_SAMPLE
-                TAKE_SAMPLE_8   BYTE_12
-                DELAY_SAMPLE    CYCLE_BTWN_SAMPLE - 1 
+SAMPLE_CYCLE_8	.macro
+
+		TAKE_SAMPLE_8		BYTE_1
+
+		DELAY_IMMEDIATE_3n 	3
+		DELAY_SAMPLE
+
+		TAKE_SAMPLE_8		BYTE_2
+
+		DELAY_IMMEDIATE_3n 	3
+		DELAY_SAMPLE
+
+		TAKE_SAMPLE_8		BYTE_3
+
+		DELAY_IMMEDIATE_3n	3
+		DELAY_SAMPLE
+
+		TAKE_SAMPLE_8		BYTE_4
+
+		DELAY_IMMEDIATE_3n	3
+		DELAY_SAMPLE
+
+		TAKE_SAMPLE_8   	BYTE_5
+
+		DELAY_IMMEDIATE_3n	3
+                DELAY_SAMPLE
+
+                TAKE_SAMPLE_8   	BYTE_6
+
+		DELAY_IMMEDIATE_3n	3
+                DELAY_SAMPLE
+
+                TAKE_SAMPLE_8   	BYTE_7
+
+		DELAY_IMMEDIATE_3n	3
+                DELAY_SAMPLE
+
+                TAKE_SAMPLE_8   	BYTE_8
+
+		DELAY_IMMEDIATE_3n	3
+                DELAY_SAMPLE
+
+		TAKE_SAMPLE_8   	BYTE_9
+
+		DELAY_IMMEDIATE_3n	3
+            	DELAY_SAMPLE
+
 		.endm
 
 ;*******************************************************
@@ -135,8 +180,8 @@ SAMPLE_CYCLE_8	.macro bit
 
 MANAGE_INTERRUPT	.macro
 			LDI32	SICR,			INT_P0_TO_P1
-			LBCO	SAMPLING_CONFIG,	SHARED_MSG,0,4	
-			.endm	
+			LBCO	SAMPLING_CONFIG,	SHARED_MSG,0,4
+			.endm
 
 	.global main
 main:
