fdc5124b2cdd69f2134b940f7d4c73f28317c39d
ZeekHuge <me.zubeen3@gmail.com>
2016-07-05 22:33:07 +0000

    Add probe and remove functions that register driver onto IIO subsystem
    This commit adds probe and remove methods that will be called by rpmsg
    bus, on detecting an appropriate device.
    
    The probe function further registers the driver to IIO subsystem.
    There are some incomplete parts of the registration and some
    incomplete structures. They will be completed as the development
    progresses.

--- driver/beaglescope_driver.c
+++ driver/beaglescope_driver.c
@@ -16,6 +16,62 @@
 #include <linux/kfifo.h>
 #include <linux/uaccess.h>
 #include <linux/poll.h>
+#include <linux/iio/iio.h>
+
+struct beaglescope_state {
+	struct rpmsg_channel *rpdev;
+};
+
+
+static const struct iio_info beaglescope_info = {
+};
+
+
+static int beaglescope_driver_probe (struct rpmsg_channel *rpmsg_dev)
+{
+	int ret;
+	struct iio_dev *indio_dev;
+	struct beaglescope_state *st;
+	struct rpmsg_device_id *id;
+
+	dev_dbg(&rpmsg_dev->dev, "Driver probed");
+
+	indio_dev = devm_iio_device_alloc(&rpmsg_dev->dev, sizeof(*st));
+	if (!indio_dev) {
+		ret = -ENOMEM;
+		goto error_ret;
+	}
+
+	id = &rpmsg_dev->id;
+	st = iio_priv(indio_dev);
+
+	dev_set_drvdata(&rpmsg_dev->dev, indio_dev);
+
+	indio_dev->dev.parent = &rpmsg_dev->dev;
+	indio_dev->name = id->name;
+	indio_dev->info = &beaglescope_info;
+
+	ret = devm_iio_device_register(&rpmsg_dev->dev, indio_dev);
+	if (ret < 0) {
+		pr_err("Failed to register with iio");
+		goto error_device_register;
+	}
+
+	return 0;
+
+error_device_register:
+error_ret:
+	return ret;
+}
+
+
+static void beaglescope_driver_remove(struct rpmsg_channel *rpmsg_dev)
+{
+	struct iio_dev *indio_dev;
+
+	indio_dev = dev_get_drvdata(&rpmsg_dev->dev);
+	devm_iio_device_unregister(&rpmsg_dev->dev, indio_dev);
+}
 
 
 static const struct rpmsg_device_id beaglescope_id[] = {
@@ -24,16 +80,19 @@ static const struct rpmsg_device_id beaglescope_id[] = {
 };
 MODULE_DEVICE_TABLE(rpmsg, beaglescope_id);
 
+
 static struct rpmsg_driver beaglescope_driver= {
 	.drv.name	= KBUILD_MODNAME,
 	.drv.owner	= THIS_MODULE,
 	.id_table	= beaglescope_id,
+	.probe		= beaglescope_driver_probe,
+	.remove		= beaglescope_driver_remove,
 };
-
 
 
 static int __init beaglescope_driver_init(void)
 {
+
 	int ret;
 
 	ret = register_rpmsg_driver(&beaglescope_driver);
@@ -41,14 +100,18 @@ static int __init beaglescope_driver_init(void)
 		pr_err("Failed to register the driver on rpmsg_bus");
 		return ret;
 	}
+
 	printk(KERN_DEBUG "Successfully registered to rpmsg_bus");
 	return 0;
+
 }
+
 
 static void __exit beaglescope_driver_exit(void)
 {
 	unregister_rpmsg_driver (&beaglescope_driver);
 }
+
 
 module_init(beaglescope_driver_init);
 module_exit(beaglescope_driver_exit);
