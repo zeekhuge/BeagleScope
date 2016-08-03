669f13fe721e37fe7b0001a1f17a5f712456a0d7
ZeekHuge <me.zubeen3@gmail.com>
2016-08-03 09:35:26 +0000

    Starting working on the parallel interface

---
+++ driver/parallel_interface.h
@@ -0,0 +1,7 @@
+/*
+ * Copyright (c) 2016 Zubeen Tolani <ZeekHuge - zeekhuge@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as published by
+ * the Free Software Foundation.
+ */
