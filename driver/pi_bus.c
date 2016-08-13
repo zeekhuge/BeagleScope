12cc71afdd1378296f7876e77d987eca63ebf5de
ZeekHuge <me.zubeen3@gmail.com>
2016-08-13 12:40:11 +0000

    Add code to register, unregister, get probed and remove function for rpmsg

--- driver/pi_bus.c
+++ driver/pi_bus.c
@@ -21,15 +21,50 @@
 #include <linux/module.h>
 #include "parallel_interface.h"
 
-static int __init pi_bus_init(void)
+static int pi_bus_rpmsg_probe (struct rpmsg_channel *rpdev)
 {
 	log_debug();
+	return 0;
+}
+
+static void pi_bus_rpmsg_remove (struct rpmsg_channel *rpdev)
+{
+	log_debug();
+}
+
+static const struct rpmsg_device_id pi_bus_rpmsg_id[] = {
+		{ .name = "pibus0" },
+		{ },
+};
+MODULE_DEVICE_TABLE(rpmsg, pi_bus_rpmsg_id);
+
+static struct rpmsg_driver pi_bus_rpmsg_driver= {
+	.drv.name	= KBUILD_MODNAME,
+	.drv.owner	= THIS_MODULE,
+	.id_table	= pi_bus_rpmsg_id,
+	.probe		= pi_bus_rpmsg_probe,
+	.remove		= pi_bus_rpmsg_remove,
+};
+
+static int __init pi_bus_init(void)
+{
+	int ret;
+	log_debug();
+
+	ret = register_rpmsg_driver(&pi_bus_rpmsg_driver);
+	if (ret) {
+		pr_err("pi_bus: couldnt register bus type");
+		return ret;
+	}
+
 	return 0;
 }
 
 static void __exit pi_bus_exit(void)
 {
 	log_debug();
+
+	unregister_rpmsg_driver(&pi_bus_rpmsg_driver);
 }
 
 module_init(pi_bus_init);
