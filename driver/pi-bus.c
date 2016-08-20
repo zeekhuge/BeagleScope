02e6e8faffc16f1ebde05799e407fe6f36ae9a96
ZeekHuge <me.zubeen3@gmail.com>
2016-08-20 10:56:50 +0000

    Add rpmsg-client support to the pi-bus driver.
    The pi-bus driver is the platform specific driver. The platform - beaglebone
    black is what this pi-bus driver is specifically for and is meant to use PRU as
    a parallel bus.
    
    To communicate to the PRUs from the Linux side, one need to create an rpmsg
    channel that would further communicate to the PRUs.
    
    This driver therefore needs to add support for the rpmsg-client driver. Further
    development would use this driver to communicate data to the PRU firmware.

--- driver/pi-bus.c
+++ driver/pi-bus.c
@@ -19,9 +19,42 @@
 			      __FUNCTION__)
 #define log_debug_msg(...) printk(KERN_DEBUG __VA_ARGS__ )
 
-static int __init pi_bus_init(void)
+static int pi_bus_rpmsg_probe(struct rpmsg_channel *rpdev)
 {
 	log_debug();
+
+	return 0;
+}
+
+static void pi_bus_rpmsg_remove(struct rpmsg_channel *rpdev)
+{
+	log_debug();
+}
+
+static const struct rpmsg_device_id pi_bus_rpmsg_id[] = {
+		{ .name = "pibus0" },
+		{ },
+};
+MODULE_DEVICE_TABLE(rpmsg, pi_bus_rpmsg_id);
+
+static struct rpmsg_driver pi_bus_rpmsg_driver = {
+	.drv.name	= KBUILD_MODNAME,
+	.drv.owner	= THIS_MODULE,
+	.id_table	= pi_bus_rpmsg_id,
+	.probe		= pi_bus_rpmsg_probe,
+	.remove		= pi_bus_rpmsg_remove,
+};
+
+static int __init pi_bus_init(void)
+{
+	int ret;
+	log_debug();
+
+	ret = register_rpmsg_driver(&pi_bus_rpmsg_driver);
+	if (ret) {
+		pr_err("pi_bus: couldnt register bus type");
+		return ret;
+	}
 
 	return 0;
 }
@@ -29,6 +62,8 @@ static int __init pi_bus_init(void)
 static void __exit pi_bus_exit(void)
 {
 	log_debug();
+
+	unregister_rpmsg_driver(&pi_bus_rpmsg_driver);
 }
 
 module_init(pi_bus_init);
