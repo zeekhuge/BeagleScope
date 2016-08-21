8a22e31c3b92239faaa82384cd8299c5b4b6199d
ZeekHuge <me.zubeen3@gmail.com>
2016-08-21 09:47:43 +0000

    Add struct pi_device to the parallel_interface.h
    This commit adds definition of the pi_device structure in the
    parallel_interface.h file to be used by device driver. An object of the
    pi_device structure will represent a client device that is connected on the
    pi bus and needs to be communicated with.

--- driver/parallel_interface.h
+++ driver/parallel_interface.h
@@ -27,6 +27,31 @@ struct pi_bus_host {
 #define to_pi_bus_host(__dev)\
 	container_of(__dev, struct pi_bus_ctrlr, dev);
 
+#define PI_MODALIAS_LENGTH	32
+
+/**
+ * pi_device	The structure to be used for a client device on the pi bus.
+ *
+ * @pibushost		The bus host device to which the client device is
+ *			connected.
+ * @modalias		The modalias value associated with the device node.
+ * @dev			The member device of the pi_device structure.
+ */
+struct pi_device {
+	struct pi_bus_host *pibushost;
+	char modalias[PI_MODALIAS_LENGTH];
+	struct device dev;
+};
+
+/**
+ * to_pi_device		Helper macro to get the pi_device object from its
+ *			member device object.
+ *
+ * @__dev	Pointer to the device associated with pi_device object.
+ */
+#define to_pi_device(__dev)\
+	container_of(__dev, struct pi_device, dev);
+
 extern struct pi_bus_host *pi_core_register_host(struct device *dev);
 extern int pi_core_unregister_host (struct pi_bus_host *pibushost);
 
